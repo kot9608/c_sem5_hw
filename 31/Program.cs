0e58d6f269930bdbbb0ce1a22a9362f7e7bba119
Kirill <Kot9608@mail.ru>
2023-02-11 14:43:04 +0500

    31 - upd comments

--- 31/Program.cs
+++ 31/Program.cs
@@ -6,7 +6,31 @@
 // сумма отрицательных равна -20.
 
 
-// Console.Clear();
+Console.Clear(); //чистим окно консоли
+int[] array = GetRandomArray(12, -9, 9); //вызываем метод GetRandomArray, создавая массив array, длинной 12 цифр, минимальное значение =-9, максимальное =9
+Console.WriteLine(String.Join(" ", array)); //выводим массив array на экран, перед этим преобразуя его в строку и вставляя между элементами пробел
+
+int positiveSum = 0; //объявили переменную для суммы положительных чисел
+int negativeSum = 0; //объявили переменную для суммы отрицательных чисел
+
+foreach (int el in array)//цикл, который пробегает по массиву, не может менять значения элементов в массиве
+{
+    positiveSum += el > 0 ? el : 0;//если элемент массива(el) > 0, тогда к positivSum прибавь этот el, иначе - прибавь 0
+    negativeSum += el < 0 ? el : 0;//если элемент массива(el) < 0, тогда к positivSum прибавь этот el, иначе - прибавь 0
+}
+
+Console.WriteLine($"Positive sum = {positiveSum}, negative sum = {negativeSum} "); //вывод на экран positiveSum, negativeSum
+
+int[] GetRandomArray(int size, int minValue, int maxValue)//метод для создания массива из рандомных чисел. На вход приниамет 3 значения(размер массива, минимальное значение элемента, максимальное значение элемента)
+{
+    int[] result = new int[size];//создание массива result длинной size(которую получили на входе)
+    for (int i = 0; i < size; i++)//цикл для заполнения элементов массива рандомными значениями
+    {
+        result[i] = new Random().Next(minValue, maxValue + 1); //элемент массива начиная с нуля присваивается рандомное значение от минимального до максимального. Т.к. максимальное не включено изначально, указано максимальное+1
+    }
+    return result;//возврат массива result из функции
+}
+
 // Console.Write("Введите длинну массива: ");
 // int s = int.Parse(Console.ReadLine());
 // Console.Write("Введите минимальное число для массива: ");
@@ -61,17 +85,6 @@
 
 /////
 
-int[] array = GetRandomArray(12, -9, 9);
-Console.WriteLine(String.Join(" ", array));
-
-int positiveSum = 0;
-int negativeSum = 0;
-
-foreach (int el in array)//[1, 2, 3, 4, -5, 1, -3] el = -5
-{
-    positiveSum += el > 0 ? el : 0;//ps += -5 > 0? 2 : 0;
-    negativeSum += el < 0 ? el : 0;
-}
 //  for (int i = 0;i < array.Length; i++){
 //     int el = array[i];
     // if(el > 0)
@@ -79,15 +92,3 @@ foreach (int el in array)//[1, 2, 3, 4, -5, 1, -3] el = -5
     // if(el < 0)
     //     negativeSum += el;
 // } 
-Console.WriteLine($"Positive sum = {positiveSum}, negative sum = {negativeSum} ");
-
-int[] GetRandomArray(int size, int minValue, int maxValue)
-{
-    int[] result = new int[size];
-    for (int i = 0; i < size; i++)
-    {
-        result[i] = new Random().Next(minValue, maxValue + 1);
-    }
-
-    return result;
-}
